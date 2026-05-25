45208f6729d2989e698cc71dfd992d918ea54434
Yelnar <yelnarmirambek@gmail.com>
2026-05-25 15:13:34 +0500

    Жаңа Google Sheets backend URL

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 
   // ====== CONFIG ======
   const WEDDING_DATE = new Date("2026-08-08T18:00:00+05:00"); // +05:00 = Almaty time
-  const BACKEND_URL = "https://script.google.com/macros/s/AKfycbyDZecGAujfX-onEdtJy0WMXO-w7vAb6yW5VEn-XsKMLmk5roDsk_yNMKJ3SvLHbgwTAw/exec";
+  const BACKEND_URL = "https://script.google.com/macros/s/AKfycby-rZ6GU_lk9ZWTzvkb8wcDYjKdjCh8ofdPBvmPM_Tsdm9bonJ1GUtbQpXHmtYsXeNrlg/exec";
   const AUDIO_VOLUME = 0.3;
 
   // ====== ELEMENTS ======
